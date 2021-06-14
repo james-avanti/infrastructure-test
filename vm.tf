b65f5485b6f49114d9d4fb94b764868083e0524a
James White <jwhite@avanti.ca>
2021-06-14 14:33:02 -0600

    VM push

---
+++ vm.tf
@@ -0,0 +1,25 @@
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.RG_James_Dev.name
+  is_windows_image    = true
+  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+  admin_password      = "ComplxP@ssw0rd!"
+  vm_os_simple        = "WindowsServer"
+  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.RG_James_Dev]
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.RG_James_Dev.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.RG_James_Dev]
+}
+
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
+}
