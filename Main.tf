c75261910b028ae75d38b4a39b00c7fed1e59fc4
James White <jwhite@avanti.ca>
2021-06-14 10:22:18 -0600

    RG Creation

--- Main.tf
+++ Main.tf
@@ -1,13 +1,9 @@
 
+provider "azurerm" (
 
- module "windowsservers" {
-  source              = "Azure/compute/azurerm"
-   resource_group_name = azurerm_resource_group.corenetwork-rg.name
-   is_windows_image    = true
-   vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-   admin_password      = "yP0#^wR5n=&3a7K3U*-x"
-  vm_os_simple        = "WindowsServer"
-   vnet_subnet_id      = module.vnet.vnet_subnets[0]
+)
+resoruce "azurerm_resource_group" "rg"{
 
-   depends_on = [azurerm_resource_group.corenetwork-rg]
- }
+  name = "JamesRG"
+  location "Canada Central"
+}
