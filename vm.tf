6894725afaba9e8170abb9b4fc05f67b0583f7ce
James White <jwhite@avanti.ca>
2021-06-15 10:42:42 -0600

    updated name

--- vm.tf
+++ vm.tf
@@ -2,7 +2,7 @@ module "windowsservers" {
   source              = "Azure/compute/azurerm"
   resource_group_name = azurerm_resource_group.RG_James_Dev.name
   is_windows_image    = true
-  vm_hostname         = "az01-dsbx-genr01" // line can be removed if only one VM module per resource group
+  vm_hostname         = "az01-dsb-genr01" // line can be removed if only one VM module per resource group
   admin_password      = "ComplxP@ssw0rd!"
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
