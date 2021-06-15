c53030fe60f119d0475ac15febae49044e9347b2
James White <jwhite@avanti.ca>
2021-06-15 08:59:39 -0600

    testing VMSize and diag settings

--- vm.tf
+++ vm.tf
@@ -2,12 +2,13 @@ module "windowsservers" {
   source              = "Azure/compute/azurerm"
   resource_group_name = azurerm_resource_group.RG_James_Dev.name
   is_windows_image    = true
-  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+  vm_hostname         = "az01-dsbx-genr01" // line can be removed if only one VM module per resource group
   admin_password      = "ComplxP@ssw0rd!"
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = module.network.vnet_subnets[0]
-
+  boot_diagnostics    = true
+  vm_size             = "Standard D16s v3"
   depends_on = [azurerm_resource_group.RG_James_Dev]
 }
 
